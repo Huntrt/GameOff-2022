34781cbdbc569751d8945d66416d03643e70c864
Huntrt <huntrt.business@outlook.com>
2022-11-15 14:24:21 +0700

    Caster will now has an object pool of all strike it has created

--- The Apocalypse Constructor/Assets/Scripts/Combat/Tower_Caster.cs
+++ The Apocalypse Constructor/Assets/Scripts/Combat/Tower_Caster.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower_Caster : MonoBehaviour
 {
 	Tower tower;
+	//Cache all the strike this caster has create
+	public List<Tower_Strike> strikes;
 
 	void Reset() 
 	{
@@ -17,4 +20,48 @@ public class Tower_Caster : MonoBehaviour
 	protected virtual void Attack() {}
 
 	void OnDisable() {tower.onAttack += Attack;}
+
+	protected virtual Tower_Strike CreateStrike(GameObject striking, Transform at)
+	{
+		//Go through all the strike has cache
+		for (int s = 0; s < strikes.Count; s++)
+		{
+			//Get this strike has cache
+			Tower_Strike strike = strikes[s];
+			//If this cached strike is unactive
+			if(!strike.gameObject.activeInHierarchy)
+			{
+				//Set the strike damage to be the tower damage
+				strike.damage = tower.damage;
+				//Set the strike position and rotation to be given transform
+				strike.transform.position = at.position;
+				strike.transform.rotation = at.rotation;
+				//Active the strike then return it
+				strike.gameObject.SetActive(true); return strike;
+			}
+		}
+
+		///When need more strike
+		//Create an new strike at given transform then cache the strike component
+		Tower_Strike newStrike = Instantiate(striking).GetComponent<Tower_Strike>();
+		//Deactive the new strike for waiting to assign it stats
+		newStrike.gameObject.SetActive(false);
+		//Set new strike caster to be this caster
+		newStrike.caster = this;
+		//Set the new strike damage to be the tower damage
+		newStrike.damage = tower.damage;
+		//Set the new strike position and rotation to be given transform
+		newStrike.transform.position = at.position;
+		newStrike.transform.rotation = at.rotation;
+		//Cache the newly create strike
+		strikes.Add(newStrike);
+		//Active the new strike then return it
+		newStrike.gameObject.SetActive(true); return newStrike;
+	}
+
+	public void StrikeOver(Tower_Strike strike)
+	{
+		//Move the strike over to be first cache since it now available
+		strikes.Insert(0, strike); strikes.Remove(strike);
+	}
 }
--- The Apocalypse Constructor/Assets/Scripts/Combat/Tower_Strike.cs
+++ The Apocalypse Constructor/Assets/Scripts/Combat/Tower_Strike.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Tower_Strike : MonoBehaviour
 {
+	public Tower_Caster caster;
     public float damage;
 
 	//Hurt the given entity with strike damage
@@ -14,5 +15,14 @@ public class Tower_Strike : MonoBehaviour
 	public virtual void Healing(GameObject entity)
 	{
 		entity.GetComponent<Entity>().Heal(damage);
+	}
+	
+	//When the strike are over
+	public virtual void Over()
+	{
+		//Deactive the strike
+		gameObject.SetActive(false);
+		//The strike of caster are now over
+		caster.StrikeOver(this);
 	}
 }
