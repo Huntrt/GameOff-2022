a139265499a75b42b534cc348a24dc46e7797f5a
Huntrt <huntrt.business@outlook.com>
2022-11-19 22:51:52 +0700

    Tower stats now handle stats scaling

--- The Apocalypse Constructor/Assets/Scripts/Structures/Tower.cs
+++ The Apocalypse Constructor/Assets/Scripts/Structures/Tower.cs
@@ -16,6 +16,7 @@ public class Tower : Structure
 		public float DPS;
 		public float damage, rate, range;
 		public float rateTimer {get {return (float)System.Math.Round(1/rate,1);}}
+		public float Scale(float stat, float scaling) {return (scaling /100) * stat;}
 	}
 
 	void OnValidate() 
