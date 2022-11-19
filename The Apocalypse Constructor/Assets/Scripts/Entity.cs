0a780bdbe7ca2727b9039185cff7a14e0c6ea901
Huntrt <huntrt.business@outlook.com>
2022-11-19 11:22:18 +0700

    Entity now send event when it got hurt or heal

--- The Apocalypse Constructor/Assets/Scripts/Entity.cs
+++ The Apocalypse Constructor/Assets/Scripts/Entity.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 { 
 	public float maxHealth;
-	[SerializeField] protected float health;
+	public float health;
+	public OnHealth onHurt, onHeal;
+	public delegate float OnHealth(float amount);
 
 	protected virtual void OnEnable()
 	{
@@ -14,6 +16,8 @@ public class Entity : MonoBehaviour
 	{
 		//Reduce health with amount got hurt
 		health -= amount;
+		//Call hurt event with damage has take
+		onHurt?.Invoke(amount);
 		//If out of health
 		if(health <= 0)
 		{
@@ -25,6 +29,8 @@ public class Entity : MonoBehaviour
 	{
 		//Increase health with amount got heal
 		health += amount;
+		//Call heal event with damage has take
+		onHeal?.Invoke(amount);
 		//Cap health from going beyond max health
 		health = Mathf.Clamp(health, 0, maxHealth);
 	}
