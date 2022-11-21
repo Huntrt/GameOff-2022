7aa15313811cd176a7207566fc0a49e75742b3c2
Huntrt <huntrt.business@outlook.com>
2022-11-22 01:39:59 +0700

    Stop by set velocity to 0 instead of conditional

--- The Apocalypse Constructor/Assets/Scripts/Enemies/Enemy_MoveWalker.cs
+++ The Apocalypse Constructor/Assets/Scripts/Enemies/Enemy_MoveWalker.cs
@@ -10,9 +10,9 @@ public class Enemy_MoveWalker : Enemy_Move
 	void FixedUpdate()
 	{
 		//Current velocity as speed
-		float velocity = speed;
-		//Stop if encounter something
-		if(encounter) velocity = 0;
+		float velocity = movementSpeed;
+		//Stop if caster detected something
+		if(caster.detected) velocity = 0;
 		//Move the enemy in the red arrow with velocity has set
 		rb.MovePosition(rb.position + ((Vector2)transform.right * velocity) * Time.fixedDeltaTime);
 	}
