685d941686b9621e6bdcb1f15c0471f270a9860f
Huntrt <huntrt.business@outlook.com>
2022-11-21 15:44:35 +0700

    Use velocity zero to stop walker

--- The Apocalypse Constructor/Assets/Scripts/Enemies/Enemy_MoveWalker.cs
+++ The Apocalypse Constructor/Assets/Scripts/Enemies/Enemy_MoveWalker.cs
@@ -9,9 +9,11 @@ public class Enemy_MoveWalker : Enemy_Move
 
 	void FixedUpdate()
 	{
+		//Current velocity as speed
+		float velocity = speed;
 		//Stop if encounter something
-		if(encounter) return;
-		//Move the enemy in the red arrow with speed has set
-		rb.MovePosition(rb.position + ((Vector2)transform.right * speed) * Time.fixedDeltaTime);
+		if(encounter) velocity = 0;
+		//Move the enemy in the red arrow with velocity has set
+		rb.MovePosition(rb.position + ((Vector2)transform.right * velocity) * Time.fixedDeltaTime);
 	}
 }
