f517a504a1f5f165311b71d2b7b497b1686b7f9f
Huntrt <huntrt.business@outlook.com>
2022-11-22 17:19:22 +0700

    Enemy now can display dps

--- The Apocalypse Constructor/Assets/Scripts/Enemies/Enemy.cs
+++ The Apocalypse Constructor/Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,12 @@ public class Enemy : Entity
 {
 	public Combats.Stats stats;
 
+	void OnValidate() 
+	{
+		//Update stats DPS
+		stats.DPS = (float)System.Math.Round(stats.damage / stats.rateTimer,2);
+	}
+
 	protected override void OnEnable()
 	{
 		base.OnEnable();
