b392aea71cf020d906f7eec9ac955c6121d6f5f2
Huntrt <huntrt.business@outlook.com>
2022-11-25 20:10:10 +0700

    Spawn rate increase with each day

--- The Apocalypse Constructor/Assets/Scripts/General/Difficulties.cs
+++ The Apocalypse Constructor/Assets/Scripts/General/Difficulties.cs
@@ -7,13 +7,42 @@ public class Difficulties : MonoBehaviour
 	#endregion
 
 	[SerializeField] DaysManager days;
-	[Header("Enemy Spawning")]
-	[SerializeField] EnemiesSpawner spawner;
-	[Tooltip("The bigger the number the rarer enemy could spawn")] [SerializeField] 
-	float spawnBoost;
+
+	public EnemiesSpawning spawning; [System.Serializable] public class EnemiesSpawning 
+	{
+		[SerializeField] EnemiesSpawner spawner;
+		[Tooltip("The bigger the number the rarer enemy could spawn")] [SerializeField] float spawnBoost;
+		[SerializeField] float spawnRateGrowEveryDay;
+		[SerializeField] float spawnRateGrowthFor;
+		
+		public void ScaleEnemySpawnRarity()
+		{
+			//Get spawning difficulty by multiply days with boost 
+			float diff = i.days.counter * spawnBoost;
+			//Go through all the enemy could spawn
+			for (int s = 0; s < spawner.spawns.Length; s++)
+			{
+				//Save this spawning
+				EnemiesSpawner.EnemySpawning spawn = spawner.spawns[s];
+				//Scaled initial rarity using decilog scale that take into account difficulty
+				spawn.scaledRarity += diff * Mathf.Log(spawn.initialRarity) / Unity.Mathematics.math.LN10;
+				//The final rarity will be use are the sum of scaled and initial
+				spawn.finalRarity = spawn.initialRarity + spawn.scaledRarity;
+			}
+		}
+
+		public void GrowthSpawnRate()
+		{
+			//When every set day has pass
+			if(i.days.counter % spawnRateGrowEveryDay == 0)
+			{	
+				//Increase spawn rate with an set amount
+				spawner.spawnRate += spawnRateGrowthFor;
+			}
+		}
+	}
 
 	void OnEnable() {DaysManager.i.onCycle += WhenDayCycleChange;}
-
 	void OnDisable() {DaysManager.i.onCycle -= WhenDayCycleChange;}
 
 	void WhenDayCycleChange(bool night)
@@ -22,28 +51,14 @@ public class Difficulties : MonoBehaviour
 		if(!night)
 		{
 			//Scale the rairty of enemy spawning
-			ScaleEnemySpawnRarity();
+			spawning.ScaleEnemySpawnRarity();
+			//Begin growing spawn rate
+			spawning.GrowthSpawnRate();
 		}
 		//If cycle to the night
 		if(night)
 		{
 
-		}
-	}
-
-	void ScaleEnemySpawnRarity()
-	{
-		//Get spawning difficulty by multiply days with boost 
-		float diff = days.counter * spawnBoost;
-		//Go through all the enemy could spawn
-		for (int s = 0; s < spawner.spawns.Length; s++)
-		{
-			//Save this spawning
-			EnemiesSpawner.EnemySpawning spawn = spawner.spawns[s];
-			//Scaled initial rarity using decilog scale that take into account difficulty
-			spawn.scaledRarity += diff * Mathf.Log(spawn.initialRarity) / Unity.Mathematics.math.LN10;
-			//The final rarity will be use are the sum of scaled and initial
-			spawn.finalRarity = spawn.initialRarity + spawn.scaledRarity;
 		}
 	}
 }
