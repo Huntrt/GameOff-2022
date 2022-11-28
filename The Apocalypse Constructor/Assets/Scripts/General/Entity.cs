de94f93fc7d2a417d1ee0701891043fc538ae28e
Huntrt <huntrt.business@outlook.com>
2022-11-28 20:36:42 +0700

    Entity handl grow max health and heal health has grow

--- The Apocalypse Constructor/Assets/Scripts/General/Entity.cs
+++ The Apocalypse Constructor/Assets/Scripts/General/Entity.cs
@@ -3,14 +3,29 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 {
 	public float maxHealth;
-	[SerializeField] float health; public float Health {get{return health;}}
+	[SerializeField] float initialMaxHP; public float InitialMaxHP {get => initialMaxHP;}
+	[SerializeField] float growthMaxHP; public float GrowthMaxHP {get => growthMaxHP;}
+	public float finalMaxHP;
+	[SerializeField] float health; public float Health {get => health;}
+
 	public OnHealth onHurt, onHeal, onDeath;
 
 	public delegate void OnHealth(float amount);
 
 	protected virtual void OnEnable()
 	{
-		health = maxHealth;
+		
+	}
+	
+	//? Since entity auto levelup from 0 when enable so this will also heal full hp since final start from 0 
+	public void GrowingHealth(int level)
+	{
+		//Save the final max hp brfore grow it
+		float preFinal = finalMaxHP;
+		//Growth the initial max hp to get the final max hp
+		finalMaxHP = initialMaxHP + (((growthMaxHP / 100) * initialMaxHP) * level);
+		//Heal for the amount of max hp got increase
+		Heal(finalMaxHP - preFinal);
 	}
 
 	public virtual void Hurt(float amount)
@@ -35,6 +50,7 @@ public class Entity : MonoBehaviour
 
 	public virtual void Die()
 	{
-		
+		//Reset the final max hp to 0
+		finalMaxHP -= finalMaxHP;
 	}
 }
